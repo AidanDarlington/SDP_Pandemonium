ac0c23b1a9fec78d07b71c22d71b5886bf50b0f1
Chetan Dev <chetandev48@gmail.com>
2023-06-06 23:06:00 +1200

    TDD_Inventory
    TDD for inventory

---
+++ Pandemonium/Intermediate/Build/Win64/PandemoniumEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.31.31104
+#include "C:/Users/socce/Documents/GitHub/SDP_Pandemonium/Pandemonium/Intermediate/Build/Win64/PandemoniumEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
---
+++ Pandemonium/Intermediate/Build/Win64/UnrealEditor/Inc/Pandemonium/UHT/StaminaCalc.generated.h
@@ -0,0 +1,112 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "StaminaCalc.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PANDEMONIUM_StaminaCalc_generated_h
+#error "StaminaCalc.generated.h already included, missing '#pragma once' in StaminaCalc.h"
+#endif
+#define PANDEMONIUM_StaminaCalc_generated_h
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_SPARSE_DATA
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execFullStamina); \
+	DECLARE_FUNCTION(execChargingStamina); \
+	DECLARE_FUNCTION(execNoStamina); \
+	DECLARE_FUNCTION(execUsingStamina);
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execFullStamina); \
+	DECLARE_FUNCTION(execChargingStamina); \
+	DECLARE_FUNCTION(execNoStamina); \
+	DECLARE_FUNCTION(execUsingStamina);
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_ACCESSORS
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUStaminaCalc(); \
+	friend struct Z_Construct_UClass_UStaminaCalc_Statics; \
+public: \
+	DECLARE_CLASS(UStaminaCalc, USaveGame, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Pandemonium"), NO_API) \
+	DECLARE_SERIALIZER(UStaminaCalc)
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_INCLASS \
+private: \
+	static void StaticRegisterNativesUStaminaCalc(); \
+	friend struct Z_Construct_UClass_UStaminaCalc_Statics; \
+public: \
+	DECLARE_CLASS(UStaminaCalc, USaveGame, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Pandemonium"), NO_API) \
+	DECLARE_SERIALIZER(UStaminaCalc)
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UStaminaCalc(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UStaminaCalc) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UStaminaCalc); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UStaminaCalc); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UStaminaCalc(UStaminaCalc&&); \
+	NO_API UStaminaCalc(const UStaminaCalc&); \
+public: \
+	NO_API virtual ~UStaminaCalc();
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UStaminaCalc(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UStaminaCalc(UStaminaCalc&&); \
+	NO_API UStaminaCalc(const UStaminaCalc&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UStaminaCalc); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UStaminaCalc); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UStaminaCalc) \
+	NO_API virtual ~UStaminaCalc();
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_13_PROLOG
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_SPARSE_DATA \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_RPC_WRAPPERS \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_ACCESSORS \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_INCLASS \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_SPARSE_DATA \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_ACCESSORS \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_INCLASS_NO_PURE_DECLS \
+	FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h_16_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PANDEMONIUM_API UClass* StaticClass<class UStaminaCalc>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_socce_Documents_GitHub_SDP_Pandemonium_Pandemonium_Source_Pandemonium_StaminaCalc_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
